77907fd7bfcf7e12e2cf7e1c6e39c19bb89d1adc
Nikhil Rao <sourcenrao@gmail.com>
2021-02-25 17:06:58 -0500

    Add dropdown menu and styling. Begin javascript for menu.

--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,7 @@
 var button = document.getElementById("next");
 var formText = document.getElementById("formText");
+var menu = document.getElementById("menu");
+var toggle = false;
 
 var circle1 = document.getElementById("circle1");
 var circle2 = document.getElementById("circle2");
@@ -11,6 +13,8 @@ var uncheckedCircle = "fa-circle";
 var checkedCircle = "fa-check-circle";
 
 var step = 1;
+
+var stepInfo = document.getElementById("step");
 
 var circleHTML = document.getElementById("circles");
 var circlesDefault = `
@@ -43,7 +47,12 @@ var step2 = `<label for="destination_state">Destination State or Zip:</label>
 <br>
 <button onclick="nextStep()" type="button" id="next">Next</button>`;
 
-var step3 = `<p>Your estimated utility bill is 222.22<br>
+var origin_state;
+var destination_state;
+var current_bill;
+var estimated_bill;
+
+var step3 = `<p>Your estimated utility bill in ${destination_state} is ${estimated_bill}<br>
 This may or may not be accurate!</p>
 <br>
 <button onclick="nextStep()" type="button" id="next">Restart</button>`;
@@ -51,6 +60,9 @@ This may or may not be accurate!</p>
 function nextStep() {
     if(step == 1){
         step = 2;
+        origin_state = document.getElementById("origin_state").value;
+        current_bill = document.getElementById("origin_bill").value;
+        stepInfo.innerHTML = "Step 2: Destination Info";
         formText.innerHTML = step2;
         formText = document.getElementById("formText");
         console.log(circle1);
@@ -62,6 +74,9 @@ function nextStep() {
     }
     if(step == 2){
         step = 3;
+        destination_state = document.getElementById("destination_state").value;
+        estimated_bill = current_bill * 2;
+        stepInfo.innerHTML = "Your Estimate";
         formText.innerHTML = step3;
         formText = document.getElementById("formText");
         circle2.classList.remove(uncheckedCircle);
@@ -74,6 +89,7 @@ function nextStep() {
         step = 1;
         formText.innerHTML = step1;
         formText = document.getElementById("formText");
+        stepInfo.innerHTML = "Step 1: Origin Details";
         circleHTML.innerHTML = circlesDefault;
         circle1 = document.getElementById("circle1");
         circle2 = document.getElementById("circle2");
@@ -81,3 +97,21 @@ function nextStep() {
         return;
     }
 };
+
+function dropMenu(){
+    if(toggle = false){
+        toggle = true;
+        menu.style.display = "inline-block";
+    }
+    else {
+        toggle = false;
+        menu.style.display = "none";
+    }
+};
+
+document.getElementById("page").onclick(function(){
+    if(toggle){
+        toggle = false;
+        menu.style.display = "none";
+    }
+});
