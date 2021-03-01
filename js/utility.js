12d70503394404f254d63ca64a69a284bfe71595
Nikhil Rao <sourcenrao@gmail.com>
2021-03-01 17:43:03 -0500

    Form validation for step 1 and 2. Form resets if step 2 validation fails?

--- js/utility.js
+++ js/utility.js
@@ -31,38 +31,48 @@ var circlesDefault = `
 `;
 
 var step1 = `<label for="origin_state">Current State or Zip:</label>
-<input type="text" id="origin_state" name="origin_state" placeholder="CA">
+<input type="text" id="origin_state" name="origin_state" placeholder="CA" required>
 
 <label for="origin_bill">Total Utilities:</label>
-<input type="text" id="origin_bill" name="origin_bill" placeholder="123.45">
+<input type="text" id="origin_bill" name="origin_bill" placeholder="123.45" required>
+
+<label for="origin_sqft">Square Footage:</label>
+<input type="text" id="origin_sqft" name="origin_sqft" placeholder="(Optional)">
 
 <br>
 <input type="submit" value="Next" onclick="nextStep()" id="next">`;
 
 var step2 = `<label for="destination_state">Destination State or Zip:</label>
-<input type="text" id="destination_state" name="destination_state" placeholder="NY">
+<input type="text" id="destination_state" name="destination_state" placeholder="NY" required>
 
-<label for="square_ft_change">Change in Sq. Ft.:</label>
+<label for="square_ft_change">New Square Footage:</label>
 <input type="text" id="square_ft_change" name="square_ft_change" placeholder="(Optional)">
 
 <br>
 <input type="submit" value="Next" onclick="nextStep()" id="next">`;
 
+// Stored Variables
+
 let origin_state = 'CA';
 let destination_state = 'NY';
-let current_bill = 123.45;
+let origin_bill = 123.45;
 let estimated_bill = 0;
 
+// RegEx matching
+
+let state = /[A-Z][A-Z]/;
+let zip = /\d\d\d\d\d/;
+let dollar = /^\$?\d+([.,]\d\d)?$/;
 
 function nextStep() {
+    var valid = validateForm();
+    if(!valid){
+        return;
+    }
     if(step == 1){
         step = 2;
-        origin_state = document.getElementById("origin_state").value;
-        current_bill = document.getElementById("origin_bill").value;
         stepInfo.innerHTML = "Step 2: Destination Info";
         formText.innerHTML = step2;
-        formText = document.getElementById("formText");
-        console.log(origin_state);
         circle1.classList.remove(uncheckedCircle);
         circle1.classList.add(checkedCircle);
         circle2.classList.remove(incompleteStepCircle);
@@ -71,14 +81,12 @@ function nextStep() {
     }
     if(step == 2){
         step = 3;
-        destination_state = document.getElementById("destination_state").value;
-        estimated_bill = current_bill * 2;
+        estimated_bill = origin_bill * 2;
         stepInfo.innerHTML = "Your Estimate";
         formText.innerHTML = `<p>Your estimated utility bill in ${destination_state} is ${estimated_bill}<br>
         This may or may not be accurate!</p>
         <br>
         <input type="submit" value="Restart" onclick="nextStep()" id="next">`;
-        formText = document.getElementById("formText");
         circle2.classList.remove(uncheckedCircle);
         circle2.classList.add(checkedCircle);
         circle3.classList.remove(incompleteStepCircle);
@@ -88,15 +96,64 @@ function nextStep() {
     if(step == 3) {
         step = 1;
         formText.innerHTML = step1;
-        formText = document.getElementById("formText");
         stepInfo.innerHTML = "Step 1: Origin Details";
         circleHTML.innerHTML = circlesDefault;
-        circle1 = document.getElementById("circle1");
-        circle2 = document.getElementById("circle2");
-        circle3 = document.getElementById("circle3");
         return;
     }
 };
+
+function validateForm(){
+    if(step == 1){
+        origin_state = document.getElementById("origin_state").value.toUpperCase();
+        origin_bill = document.getElementById("origin_bill").value;
+        // Test for 2 or 5 characters
+        if(origin_state.length == 2 || origin_state.length == 5){
+            // Test for 2 letters or 5 numbers
+            if(state.test(origin_state) || zip.test(origin_state)){
+                
+            }
+            else {
+                document.getElementById("origin_state").style.boxShadow = "inset -1px 2px 5px rgba(235, 11, 11, 0.9)";
+                return false;
+            }
+        }
+        else {
+            document.getElementById("origin_state").style.boxShadow = "inset -1px 2px 5px rgba(235, 11, 11, 0.9)";
+            return false;
+        }
+        // Test for dollar value
+        if(dollar.test(origin_bill)){
+            return true;
+        }
+        else {
+            document.getElementById("origin_bill").style.boxShadow = "inset -1px 2px 5px rgba(235, 11, 11, 0.9)";
+            return false;
+        }
+    }
+
+
+    if(step == 2){
+        destination_state = document.getElementById("destination_state").value;
+        if(destination_state.length == 2 || destination_state.length == 5){
+            // Test for 2 letters or 5 numbers
+            if(state.test(origin_state) || zip.test(origin_state)){
+                return true;
+            }
+            else {
+                document.getElementById("destination_state").style.boxShadow = "inset -1px 2px 5px rgba(235, 11, 11, 0.9)";
+                return false;
+            }
+        }
+        else {
+            document.getElementById("destination_state").style.boxShadow = "inset -1px 2px 5px rgba(235, 11, 11, 0.9)";
+            return false;
+        }
+    }
+
+    if(step == 3) {
+        return true;
+    }
+}
 
 function dropMenu(){
     if(toggle == false){
@@ -109,6 +166,8 @@ function dropMenu(){
         toggle = false;
     }
 };
+
+
 
 // Close Menu when clicking outside the menu
 // main.onclick = function() {closeMenu()};
