f1d12e03072fbf5a9ed9334854708cc134c2d1e9
Nikhil Rao <sourcenrao@gmail.com>
2021-03-07 18:14:15 -0500

    Variablize css colors into :root selector. Standardize color scheme. Begin dark mode toggle function in JS.

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,15 @@
 var menu = document.getElementById("menu");
-var toggle = false;
+var menuToggle = false;
 
 function dropMenu(){
-    if(toggle == false){
+    if(menuToggle == false){
         menu.style.display = "inline-block";
-        toggle = true;
+        menuToggle = true;
         return;
     }
     else {
         menu.style.display = "none";
-        toggle = false;
+        menuToggle = false;
     }
 };
 
@@ -17,10 +17,34 @@ function dropMenu(){
 function getWindowWidth() {
     let windowWidth = window.innerWidth;
     setTimeout(function(){
-        if (windowWidth >= 800 && toggle == true) {
+        if (windowWidth >= 800 && menuToggle == true) {
             menu.style.display = 'none';
         }
     }, 100);
 }
 
 window.addEventListener('resize', getWindowWidth);
+
+// Dark Mode vars
+
+var root = document.querySelector(':root');
+var modeToggle = false;
+
+var light1 = '#bce4bc';
+var light2 = '#84d876';
+var light290 = '#84d87690';
+var dark1 = '#1a171d';
+var dark2 = 'rgba(29, 4, 31, 0.5)';
+var lightshadow = 'rgba(220, 226, 129, 0.5)';
+
+function darkMode() {
+    console.log("test");
+    if(modeToggle == false) {
+        root.style.setProperty('--foreground1', dark1);
+        root.style.setProperty('--foreground2', dark2);
+        root.style.setProperty('--foreground290', dark2);
+        root.style.setProperty('--background1', light1);
+        root.style.setProperty('--background2', light2);
+        root.style.setProperty('--shadow1', dark2);
+    }
+}
