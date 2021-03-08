3a6dc015a8cf81cdc8d1fa2b5cfc89841e665996
Nikhil Rao <sourcenrao@gmail.com>
2021-03-07 19:08:59 -0500

    Finish dark mode function. Fix step tracker h2 tag setting font weight. Improve some sizing issues and remove background texture.

--- js/dropdown.js
+++ js/dropdown.js
@@ -33,18 +33,33 @@ var modeToggle = false;
 var light1 = '#bce4bc';
 var light2 = '#84d876';
 var light290 = '#84d87690';
-var dark1 = '#1a171d';
-var dark2 = 'rgba(29, 4, 31, 0.5)';
+var dark1 = '#160c20';
+var dark2 = 'rgb(29, 4, 31)';
+var dark290 = 'rgba(29, 4, 31, .9)';
 var lightshadow = 'rgba(220, 226, 129, 0.5)';
+var darkshadow = '';
 
 function darkMode() {
-    console.log("test");
     if(modeToggle == false) {
-        root.style.setProperty('--foreground1', dark1);
-        root.style.setProperty('--foreground2', dark2);
-        root.style.setProperty('--foreground290', dark2);
-        root.style.setProperty('--background1', light1);
-        root.style.setProperty('--background2', light2);
-        root.style.setProperty('--shadow1', dark2);
+        root.style.setProperty('--foreground1', '#5E0B73');
+        root.style.setProperty('--foreground2', '#31043D');
+        root.style.setProperty('--foreground290', '#31043D90');
+        root.style.setProperty('--background1', '#DCF0DE');
+        root.style.setProperty('--background2', '#D1F2CC');
+        root.style.setProperty('--background290', '#D1F2CC90');
+        root.style.setProperty('--shadow1', '#B787C280');
+        modeToggle = true;
+        return;
+    }
+    else {
+        root.style.setProperty('--foreground1', light1);
+        root.style.setProperty('--foreground2', light2);
+        root.style.setProperty('--foreground290', light290);
+        root.style.setProperty('--background1', dark1);
+        root.style.setProperty('--background2', dark2);
+        root.style.setProperty('--background290', dark290);
+        root.style.setProperty('--shadow1', lightshadow);
+        modeToggle = false;
+        return;
     }
 }
